d9620a892dc8c880fb4af931b3b76758aa7ccf20
Andres9654 <ag8758191@gmail.com>
2024-03-30 12:44:05 -0600

    Proyecto

---
+++ java.js
@@ -0,0 +1,27 @@
+function opcionSeleccionada(opcion){
+    
+    switch(opcion){
+        case 1:
+            console.log("Inicio");
+        break;
+        case 2:
+            console.log("Pedidos");
+            break;
+        case 3:
+            console.log("Vendedores");
+            break;
+        case 4:
+            console.log("Descargas");
+            break;
+        case 5:
+            console.log("Github");
+            break;
+        case 6:
+            console.log("Email");
+            break;
+        case 7:
+            console.log("-- SALIR --");
+            break;
+    }
+
+}
